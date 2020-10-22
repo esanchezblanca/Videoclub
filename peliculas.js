f76b36560b0d1e110c8e766bba693f7ecedbf50b
Elena <esanchez6@gmail.com>
2020-10-22 23:55:55 +0200

    añadidas algunas películas

--- peliculas.js
+++ peliculas.js
@@ -1,31 +1,32 @@
 let arrPeliculas = [];
 
 class Movies { 
-    constructor(id, title, year, genre, cast) {
+    constructor(id, title, year, genre, cast, cover) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.cast = cast;
+        this.cover = cover;
        
     }
 }
 
-let m1 = new Movies("ID001", "Shrek", 2001, "animación", "Mike Myers");
-let m1 = new Movies("ID002", "El príncipe de Zamunda", 1988, "comedia", "Eddie Murphy");
+let m1 = new Movies("ID001", "Shrek", 2001, "animación", "Mike Myers", );
+let m1 = new Movies("ID002", "El príncipe de Zamunda", 1988, "comedia", "Eddie Murphy", );
 let m3 = new Movies("ID003", "Con la muerte en los talones", 1959, "suspense", "Cary Grant");
 let m4 = new Movies("ID004", "Titanic", 1997, "drama", "Leonardo diCaprio");
 let m5 = new Movies("ID005", "El caballero oscuro", 2008, "acción", "Christian Bale");
-let m6 = new Movies("Id006", "Mad Max: Fury Road", 2015 "acción", "Charlize Theron");
+let m6 = new Movies("Id006", "Mad Max: Fury Road", 2015, "acción", "Charlize Theron");
 let m7 = new Movies("ID007", "Solo en casa", 1990, "comedia", "Macaulay Culkin");
 let m8 = new Movies("ID008", "Django desencadenado", 2012, "drama", "Jamie Foxx");
-let m9 = new Movies
-let m10 = new Movies
-let m11 = new Movies
-let m12 = new Movies
-let m13 = new Movies
-let m14 = new Movies
-let m15 = new Movies
+let m9 = new Movies("ID009", "Casablanca", 1942, "drama", "Humpfrey Bogart");
+let m10 = new Movies("ID010", "IT", 1990, "terror", "Tim Curry");
+let m11 = new Movies("ID011", "Donnie Darko", 2001, "suspense", "Jake Gyllenhaal");
+let m12 = new Movies("ID012", "The Host", 2006, "suspense", "Song Kang Ho");
+let m13 = new Movies("ID013", "Parásitos", 2019, "suspense", "Song Kang Ho");
+let m14 = new Movies("ID014", "La tostadora valiente", 1989, "animación", "Toaster");
+let m15 = new Movies("ID0015", "Una jaula de grillos", 1996, "comedia", "Robin Williams");
 let m16 = new Movies
 let m17 = new Movies
 let m18 = new Movies

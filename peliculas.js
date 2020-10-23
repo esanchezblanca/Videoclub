505e777d54bea054dcfda3e8bd26b37a6a75f0db
Elena <esanchez6@gmail.com>
2020-10-23 20:15:12 +0200

    modificado array películas

--- peliculas.js
+++ peliculas.js
@@ -1,6 +1,4 @@
-let arrPeliculas = [];
-
-class Movies { 
+class Movie { 
     constructor(id, title, year, genre, cast, cover) {
         this.id = id;
         this.title = title;
@@ -8,28 +6,7 @@ class Movies {
         this.genre = genre;
         this.cast = cast;
         this.cover = cover;
-       
     }
 }
 
-let m1 = new Movies("ID001", "Shrek", 2001, "animación", "Mike Myers", "https://www.coverwhiz.com/uploads/movies/Shrek.jpg");
-let m1 = new Movies("ID002", "El príncipe de Zamunda", 1988, "comedia", "Eddie Murphy", "https://i.pinimg.com/736x/7c/cc/71/7ccc7128574285284d90f926fffcee91.jpg" );
-let m3 = new Movies("ID003", "Con la muerte en los talones", 1959, "suspense", "Cary Grant", "https://images-na.ssl-images-amazon.com/images/I/71EfF9-Yb%2BL._SL1054_.jpg");
-let m4 = new Movies("ID004", "Titanic", 1997, "drama", "Leonardo diCaprio", "https://images-na.ssl-images-amazon.com/images/I/51G13d3EwBL._AC_UL600_SR405,600_.jpg");
-let m5 = new Movies("ID005", "El caballero oscuro", 2008, "acción", "Christian Bale", "https://pics.filmaffinity.com/El_caballero_oscuro-102763119-large.jpg");
-let m6 = new Movies("Id006", "Mad Max: Fury Road", 2015, "acción", "Charlize Theron", "https://www.websweepuk.com/ebay_images/Mad-Max-Fury-Road-us-one-sheet-buy-original-movie-posters-at-starstills.jpg");
-let m7 = new Movies("ID007", "Solo en casa", 1990, "comedia", "Macaulay Culkin", "https://cartelera.elpais.com/assets/uploads/2018/12/21030155/F_12929.jpg");
-let m8 = new Movies("ID008", "Django desencadenado", 2012, "drama", "Jamie Foxx", "https://images-na.ssl-images-amazon.com/images/I/51dNfU53lNL._AC_.jpg");
-let m9 = new Movies("ID009", "Casablanca", 1942, "drama", "Humpfrey Bogart", "https://e.snmc.io/i/300/w/77330ee70810f141e075acff98852f53/6595852");
-let m10 = new Movies("ID010", "IT", 1990, "terror", "Tim Curry", "https://images-na.ssl-images-amazon.com/images/I/513lqk2Bz9L.jpg");
-let m11 = new Movies("ID011", "Donnie Darko", 2001, "suspense", "Jake Gyllenhaal", "https://pics.filmaffinity.com/Donnie_Darko-454757435-large.jpg");
-let m12 = new Movies("ID012", "The Host", 2006, "suspense", "Song Kang Ho", "https://3.bp.blogspot.com/-e97_3gfk9-A/TbASyOvM6lI/AAAAAAAAIZA/Keh3KvmlzjY/s1600/28469_thehost.png");
-let m13 = new Movies("ID013", "Parásitos", 2019, "suspense", "Song Kang Ho", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQqOXfUTEZWtFJeG2o7So-oVY0WVZBS3VLyIg&usqp=CAU");
-let m14 = new Movies("ID014", "La tostadora valiente", 1989, "animación", "Toaster", "https://pbs.twimg.com/media/EU2hmvRX0AAipi2.jpg");
-let m15 = new Movies("ID0015", "Una jaula de grillos", 1996, "comedia", "Robin Williams", "https://pics.filmaffinity.com/Una_jaula_de_grillos-445695732-large.jpg");
-let m16 = new Movies("ID016", "El Padrino", 1972, "drama", "Al Pacino", "https://pics.filmaffinity.com/El_padrino-500674179-large.jpg")
-let m17 = new Movies("ID0017", "La Vida es bella", 1999, "Roberto Benigni", "https://images-na.ssl-images-amazon.com/images/I/51XcLlHl-xL._AC_UL320_SR224,320_.jpg")
-let m18 = new Movies("ID018", "Forrest Gump", 1994, "Tom Hanks", "https://pics.filmaffinity.com/Forrest_Gump-166768337-large.jpg")
-let m19 = new Movies("ID019", "Intocable", 2012, "François Cluzet", "https://pics.filmaffinity.com/Intocable-218166359-large.jpg")
-let m20 = new Movies("ID020", "Cadena Perpetua", 1995, "Morgan Freeman", "https://images-na.ssl-images-amazon.com/images/I/71cyqt3GXYL._SL1071_.jpg")
-
+module.export = Movie;
